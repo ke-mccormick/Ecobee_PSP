cd4456117c65634dcc502c9c7873f5fce5932880
ke-mccormick <ke-mccormick@wiu.edu>
2017-06-25 03:45:53 -0500

    Initial Code and Files

---
+++ README.md
@@ -0,0 +1,58 @@
+# Ecobee_PSP
+
+This program is for anyone using an Ecobee3 or similar model thermostat that supports the Ecobee API and is a Power Smart Pricing customer.
+
+https://www.ecobee.com/
+
+https://www.powersmartpricing.org/
+
+The purpose of this program is to set the Ecobee thermostat to Away if the PSP price is greater than a user defined pricing point,
+and will resume the programmed schedule when the price is equal or less than the user defined pricing point.
+
+The program also requires IFTTT webhooks called PSP_Price_High and PSP_Price_Low for notifications.
+Make sure the webhook names match the names listed above and configure the applets how ever you like be notified.
+https://ifttt.com/
+
+IFTTT can be slow, so do not expect notification right away.
+I have asked Power Smart Pricing to integrate with IFTTT, but that has not happened yet.
+
+We could use IFTTT to control the Ecobee thermostat, but this can take 30 minutes to update after trigger.
+Using the Ecobee API the update happens nearly instantly after triggering.
+
+**Notice:  The files client_id.txt, IFTTT_id.txt, and tokens.txt are only examples and contain bogas data.
+These files will not work, please read below on how to configure them.
+
+IFTTT_id.txt contains the key hash for the webhooks and can be found in Maker Webhooks settings.
+
+client_id.txt contains your Ecobee client ID for this application and is found on your Ecobee thermostat after authorizing the application.
+
+tokens.txt is a JSON file that contains the tokens for access and token refresh provided by Ecobee.
+
+You will need to get the information contained in the files from Ecobee and IFTTT for your specific installation.
+
+Please begin with the Ecobee Developers page.
+https://www.ecobee.com/home/developer/api/introduction/index.shtml
+
+If you have questions, the Ecobee Developer Community maybe able to help you.
+http://developer.ecobee.com/api
+
+This application also requires the use of CURL, so be sure to have CURL installed.
+https://curl.haxx.se/
+
+All files need to be in same directory.
+
+Usage: psp.py -n -p price
+	-n			Optional: 	Enables IFTTT notification.
+	-p price		Required: 	Defines the maximum Power Smart Pricing price.
+	
+Example: psp.py -n -p 4.3
+
+If you do not want IFTTT notifications then leave off the -n parameter.
+
+Max Price is the Maximum Power Smart Pricing price per kWh you are willing to pay with HVAC system running normally.
+
+If the price becomes higher than the Max Price your thermostat will be put in away mode, reducing run time until the Power Smart Pricing price is reduced to or less than the Max Price.
+
+You may use the Ameren Electric Supply price or whatever price you prefer.
+
+https://www.pluginillinois.org/FixedRateBreakdownAmeren.aspx
---
+++ psp.py
@@ -0,0 +1,192 @@
+from subprocess import check_output
+import datetime
+import getopt
+import json
+import os
+import sys
+import time
+import urllib.request
+
+# Returns client id from file client_id.txt as string.
+def get_file_path(file):
+    file_path = os.path.join(sys.path[0], file)
+    return file_path
+
+# Returns JSON string value from JSON file as string.
+def get_JSON_string_value(file, string):
+    key_file = open(get_file_path(file), 'r')
+    key_data = json.loads(key_file.read())
+    return key_data[string]
+
+# Returns client id from file client_id.txt as string.
+def get_client_id():
+    client_file = open(get_file_path('client_id.txt'), 'r')
+    client_id = client_file.read()
+    return client_id
+
+# Returns refresh token from JSON file tokens.txt as string.
+def get_refresh_token():
+    refresh_token = get_JSON_string_value('tokens.txt','refresh_token')
+    return refresh_token
+
+# Returns refresh token from JSON file tokens.txt as string.
+def get_access_token():
+    access_token = get_JSON_string_value('tokens.txt','access_token')
+    return access_token
+
+# Excutes CURL command to refresh keys stored in JSON file tokens.txt
+def refresh_keys():
+    refresh_token = get_refresh_token()
+    client_id = get_client_id()
+    refresh = 'curl -s -X POST "https://api.ecobee.com/token?grant_type=refresh_token&refresh_token=' + refresh_token + '&client_id=' + client_id + '" -o tokens.txt'
+    check_output(refresh, shell=True).decode()
+    return None
+
+# Returns the current PSP hourly price as string.
+def get_psp_price():
+    psp_request = urllib.request.Request('https://www.powersmartpricing.org/psp/servlet?type=instantHOURlyfullsite')
+    psp_response = urllib.request.urlopen(psp_request)
+    psp_price_string = psp_response.read()
+    psp_price_elements = psp_price_string.split()
+    current_psp_price = psp_price_elements[3].decode()
+    return current_psp_price
+
+# Execute Ecobee command file.
+def execute_Ecobee_command_file(command_file):
+    refresh_keys() # Access Tokens expire hour, best to refresh before execution.
+    access_token = get_access_token()
+    file_path = get_file_path(command_file)
+    command = 'curl -s -X POST --data-urlencode @' + file_path + ' -H "Content-Type: application/json;charset=UTF-8" -H "Authorization: Bearer ' + access_token + '" "https://api.ecobee.com/1/thermostat?format=json"'
+    check_output(command, shell=True).decode()
+    return None
+
+# IFTTT Notification
+def IFTTT_notify(message):
+    IFTTT_file = open(get_file_path('IFTTT_id.txt'), 'r')
+    IFTTT_id = IFTTT_file.read()
+    notify = 'curl -s -X POST https://maker.ifttt.com/trigger/' + message + '/with/key/' + IFTTT_id
+    check_output(notify, shell=True).decode()
+    return None
+
+# Set Ecobee away indefinitely.
+def set_away_indefinitely():
+    execute_Ecobee_command_file('indefinite_away.txt')
+    print('Setting Away Indefinitely')
+    return None
+
+# Set Ecobee resume program.
+def set_resume_program():
+    execute_Ecobee_command_file('resume_program.txt')
+    print('Resuming Programming')
+    return None
+
+# Print Press ENTER to exit message and exit after user hits ENTER.
+def press_ENTER_exit_message():
+    input('\n' + 'Press ENTER to exit')
+    sys.exit()
+    return None
+
+# Print file usage statement.
+def usage():
+    print('Usage: psp.py -n -p price' + '\n')
+    print('-n        Optional: Enables IFTTT notification.'+ '\n')
+    print('-p price  Required: Defines the maximum Power Smart Pricing price.'+ '\n')
+    return None
+
+# Clears the output window.
+def clear_output_window():
+    os.system('cls' if os.name=='nt' else 'clear')
+    return None
+
+# Returns current system time.
+def get_system_time():
+    time = datetime.datetime.time(datetime.datetime.now())
+    return time
+
+# Returns current system hour as integer.
+def get_system_hour():
+    hour = get_system_time().strftime('%H')
+    return int(hour)
+
+# Returns current system minute as integer.
+def get_system_minute():
+    minute = get_system_time().strftime('%M')
+    return int(minute)
+
+# Main.
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],'hnp:',['help','notify','price='])
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+        press_ENTER_exit_message()
+    if not opts:
+        usage()
+        press_ENTER_exit_message()
+
+    notify = False
+    max_price = -1
+
+    for opt, arg in opts:
+        if opt in ('-h','--help'):
+            usage()
+            press_ENTER_exit_message()
+        elif opt in ('-n', '--notify'):
+            notify = True
+        elif opt in ('-p', '--price'):
+            max_price = arg
+        else:
+            assert False, "unhandled option"
+
+    # Max Price must be set.
+    if(max_price == -1):
+        usage()
+        press_ENTER_exit_message()
+
+    # Keep track of last change.
+    previous_hour = -1
+    current_hour = get_system_hour()
+    current_price = float(0)
+    PSP_delay = 15
+    PSP_High = -1
+    
+    # Need loop here.
+    while(True):
+        # Check for time change.
+        while(previous_hour == current_hour):
+            time.sleep(1) # Delay for 1 seconds.
+            current_hour = get_system_hour() # Update current hour.
+        # If hour has just changed, wait for PSP website to updated price.
+        if(get_system_minute() == 0):
+            clear_output_window()
+            print('Updating Current PSP Price')
+            print('Please Wait ' + str(PSP_delay) + ' Seconds...')
+            time.sleep(PSP_delay) # Delay for PSP_delay number of seconds.
+            clear_output_window()
+        previous_hour = current_hour # Update previous hour to current hour.
+        print('Update Triggered at ' + get_system_time().strftime('%H:%M:%S')) # Display Current Time
+        print('Max Price is ' + max_price) # Print Max Price
+        current_price = float(get_psp_price()) # Update Current Price
+        print('Current Price is ' + str(current_price)) # Display Current Price
+        # If current price is cheaper than max price, resume program else set away indefinitely.
+        if(current_price <= float(max_price)):
+            print('Current Price Less Than Or Equal To Max Price')
+            set_resume_program()
+            # Notify once when price changed.
+            if(notify == True and PSP_High != 0):
+                IFTTT_notify('PSP_Price_Low')
+            PSP_High = 0
+        else:
+            print('Current Price Greater Than Max Price')
+            set_away_indefinitely()
+            # Notify once when price changed.
+            if(notify == True and PSP_High != 1):
+                IFTTT_notify('PSP_Price_High')
+            PSP_High = 1
+        print('Please Wait For Next Hourly Update...')
+
+    press_ENTER_exit_message()
+
+if __name__ == "__main__":
+    main()
